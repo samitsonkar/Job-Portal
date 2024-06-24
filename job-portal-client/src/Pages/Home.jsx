583464a9c4f60a483feca72c554aa5e869ab9344
Samit <samitsonkar0@gmail.com>
2024-06-24 14:30:41 +0530

    Half filtering service done

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Banner from '../Components/Banner'
 import Card from '../Components/Card';
 import Jobs from './Jobs';
+import Sidebar from '../sidebar/Sidebar';
 
 const Home = () => {
   const [selectedCategory,setSelectedCategory] = useState(null);
@@ -34,23 +35,27 @@ const Home = () => {
         filteredJobs = filteredItems;
       }
       if(selected){
-        filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => {
+        filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => (
           jobLocation.toLowerCase()===selected.toLowerCase()||
           parseInt(maxPrice)===parseInt(selected)||
           salaryType.toLowerCase()===selected.toLowerCase||
           employmentType.toLowerCase()===selected.toLowerCase()
-        })
+        ))
       }
+      console.log(filteredJobs)
       return filteredJobs.map((data,i) => <Card key={i} data={data}/>)
     }
 
     const result = filteredData(jobs, selectedCategory, query);
 
+
   return (
     <div>
     <Banner query={query} handleInputChange={handleInputChange}/>
     <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12'>
-      <div className='bg-white p-4 rounded'>Left</div>
+      <div className='bg-white p-4 rounded'>
+        <Sidebar handleChange={handleChange} handleClick={handleClick}/>
+      </div>
       <div className='col-span-2 bg-white p-4 rounded-sm'><Jobs result ={result}/></div>
       <div className='bg-white p-4 rounded'>Right</div>
     </div>
---
+++ job-portal-client/src/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Location from './Location'
+
+const Sidebar = ({handleChange, handleClick}) => {
+  return (
+    <div className='space-y-5'>
+        <h3 className='text-lg font-bold mb-2'>Filter</h3>
+        <Location handleChange={handleChange}/>
+    </div>
+  )
+}
+
+export default Sidebar
